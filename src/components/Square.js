8f6a4c83c806ccf721e65fdfe0995553906870db
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2021-04-25 00:59:28 +0530

    Square class state update handled on click

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+  handleSquareClick(value) {
+    this.setState({
+      value,
+    });
+  }
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button className="square" onClick={() => this.handleSquareClick('X')}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 

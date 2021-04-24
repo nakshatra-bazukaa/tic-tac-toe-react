748919277e566647b113f4248fdc879333a43c48
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2021-04-25 00:50:05 +0530

    Environment setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Game from './Game';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Game />
+      </div>
+    );
+  }
+}
+
+export default App;

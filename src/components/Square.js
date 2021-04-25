24266b9f41c163dbee95c203083f765f9a2f86e4
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2021-04-25 06:30:45 +0530

    Square changed to functional component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,19 +1,9 @@
-import React from 'react';
-
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.state.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;

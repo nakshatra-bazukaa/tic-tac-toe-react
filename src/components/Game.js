6a642c92b135050d656b0dfde3248e479e2c9018
nakshatra-bazukaa <nakshatravasugupta@gmail.com>
2021-04-25 09:24:22 +0530

    Orientation changed of board

--- src/components/Game.js
+++ src/components/Game.js
@@ -76,15 +76,15 @@ class Game extends React.Component {
 
     return (
       <div className="game">
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
         <div className="game-board">
           <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
           />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
         </div>
       </div>
     );
